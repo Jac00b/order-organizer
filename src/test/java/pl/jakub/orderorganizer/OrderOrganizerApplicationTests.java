a5d795ce4b177426fb84399fd615229163561c51
Jac00b <jakub.misiuk@gmail.com>
2021-03-22 20:48:51 +0100

    first commit, project initialization

---
+++ src/test/java/pl/jakub/orderorganizer/OrderOrganizerApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.jakub.orderorganizer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OrderOrganizerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
